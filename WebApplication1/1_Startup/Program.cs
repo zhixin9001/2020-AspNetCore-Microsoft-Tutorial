273f764782820d462c8e8ac639c173b8e62a99ce
zhixin9001 <zhixin9001@live.com>
2020-03-17 07:01:20 +0800

    3.17

---
+++ WebApplication1/1_Startup/Program.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace _1_Startup
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            //webBuilder.UseStartup<Startup>();
+        //            webBuilder.ConfigureServices(services =>
+        //            {
+        //                services.AddControllersWithViews();
+        //            }).Configure
+        //        });
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.ConfigureServices((c, services) =>
+                {
+                    services.AddControllers();
+                })
+                .Configure(app =>
+                {
+                    var env = app.ApplicationServices.GetRequiredService<IWebHostEnvironment>();
+
+                    if (env.IsDevelopment())
+                    {
+                        app.UseDeveloperExceptionPage();
+                    }
+
+                    app.UseRouting();
+
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapGet("/", async context =>
+                        {
+                            await context.Response.WriteAsync("Hello World!");
+                        });
+                    });
+                });
+            });
+    }
+}
