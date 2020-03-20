0a6f975b758d2222b9dcb93076cdd5dffe8bb9d5
dante.x.zhi <dante.x.zhi@newegg.com>
2020-03-20 13:03:20 +0800

    3.20

---
+++ WebApplication1/4_Config/Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace _4_Config
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(config =>
+                {
+                    var dict = new Dictionary<string, string>
+                    {
+                        {"key1", "value1"},
+                        {"key2", "value2"}
+                    };
+
+                    config.AddInMemoryCollection(dict);
+                    config.AddJsonFile("config1.json", optional: true, reloadOnChange: true);
+                    //config.AddCommandLine(args);
+                    //config.Sources.Clear();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ WebApplication1/4_Config/Startup.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace _4_Config
+{
+    public class Startup
+    {
+        public Startup(IConfiguration config)
+        {
+            var a = config.GetSection("key1").Value;
+            var b = config.GetSection("section0:key0").Value;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IConfiguration config)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    var b = config.GetSection("section0:key0").Value;
+                    var c = config.GetSection("section1:key1").Value;
+
+                    var d = config.GetSection("cmd").Value;
+                    var e = config.GetSection("cmd2").Value;
+                    await context.Response.WriteAsync($"Hello World!=={b}=={c}==cmd={d}==cmd2={e}");
+                });
+            });
+        }
+    }
+}
